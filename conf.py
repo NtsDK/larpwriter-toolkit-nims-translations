78ea23c7f3de1fb36c9d11537d8cbcf2c7246691
NtsDK <ghoul71@yandex.ru>
2015-11-24 22:17:16 +0500

    Updated pictures. Updated doc for recent UI changes.

--- conf.py
+++ conf.py
@@ -116,8 +116,8 @@ numfig = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # -- html_theme = 'alabaster'
-#html_theme = 'classic'
-html_theme = 'nature'
+html_theme = 'classic'
+#html_theme = 'nature'
 # -- html_theme = 'sphinx_rtd_theme'
 # -- html_theme = 'bizstyle'
 # -- html_theme = 'sphinxdoc'
